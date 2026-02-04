90316e93d510c3ab450adb5ccdedb9b0a83c8af7
RichardPC <richkejie@gmail.com>
2026-02-04 11:21:25 -0500

    update to docker run script

--- run_docker_container.sh
+++ run_docker_container.sh
@@ -1,4 +1,4 @@
-docker run -it --net=host --env="DISPLAY=host.docker.internal:0.0" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --volume="$(pwd)src:/workspace/src/rob521_labs_mount" --gpus all --name rob521_container rob521-labs-img
+docker run -it --net=host --env="DISPLAY=host.docker.internal:0.0" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --volume="$(pwd)/src:/workspace/src/rob521_labs_mount" --gpus all --name rob521_container rob521-labs-img
 
 
 # --env="DISPLAY"
