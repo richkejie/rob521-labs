2eb755309f6d9efa163197d91ca6d17a57c21ada
RichardPC <richkejie@gmail.com>
2026-02-08 13:04:57 -0500

    update follow path file path names

--- src/rob521_lab2/nodes/l2_follow_path.py
+++ src/rob521_lab2/nodes/l2_follow_path.py
@@ -29,8 +29,7 @@ ROT_DIST_MULT = .1  # multiplier to change effect of rotational distance in choo
 OBS_DIST_MULT = .1  # multiplier to change the effect of low distance to obstacles on a path
 MIN_TRANS_DIST_TO_USE_ROT = TRANS_GOAL_TOL  # m, robot has to be within this distance to use rot distance in cost
 # PATH_NAME = 'path.npy'  # saved path from l2_planning.py, should be in the same directory as this file
-# PATH_NAME = 'will_path.npy'
-PATH_NAME = 'myhal_path.npy'
+PATH_NAME = 'will_path.npy'
 
 # here are some hardcoded paths to use if you want to develop l2_planning and this file in parallel
 # TEMP_HARDCODE_PATH = [[2, 0, 0], [2.75, -1, -np.pi/2], [2.75, -4, -np.pi/2], [2, -4.4, np.pi]]  # almost collision-free
